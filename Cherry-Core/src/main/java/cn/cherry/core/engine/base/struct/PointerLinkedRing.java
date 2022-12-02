7a5a44607a1931f944cb6a25aacc63bb8568c662
RealDragonking <51237027+sqddk@users.noreply.github.com>
2022-12-02 15:40:28 +0800

    PointerLinkedRing得到实现

--- Cherry-Core/src/main/java/cn/cherry/core/engine/base/struct/PointerLinkedRing.java
+++ Cherry-Core/src/main/java/cn/cherry/core/engine/base/struct/PointerLinkedRing.java
@@ -1,10 +1,69 @@
 package cn.cherry.core.engine.base.struct;
 
+import java.util.List;
+
 /**
  * {@link PointerLinkedRing}是一种环形链表。<br/>
- * 在{@link PointerLinkedRing}的具体实现类中，不断地{@link #moveNext()}将回到起点处的节点位置。
+ * 在{@link PointerLinkedRing}中，不断地{@link #moveNext()}将回到起点处的节点位置。
  * 在{@link PointerLinkedRing}中，所有的节点{@link Node}应当在构造方法中就被初始化完成并建立指向关系。
+ * 在这之后，所有节点都无法被修改。
  *
  * @author realDragonKing
  */
-public interface PointerLinkedRing<E> extends PointerLinked<E> {}
+public class PointerLinkedRing<E> implements PointerLinked<E> {
+
+    private final int size;
+
+    private Node<E> pointer;
+
+    public PointerLinkedRing(List<E> values) {
+        if (values.size() == 0) {
+            throw new IllegalArgumentException("至少要有一个节点值来完成初始化！");
+        }
+        Node<E> prev = new Node<>(values.get(0));
+        Node<E> next;
+        for (E value : values) {
+            next = new Node<>(value);
+            prev.setNext(next);
+            next.setPrev(prev);
+            prev = next;
+        }
+        this.pointer = prev;
+        this.size = values.size();
+    }
+
+    /**
+     * @return pointer指针所指的节点的值
+     */
+    @Override
+    public E getValue() {
+        return this.pointer.getValue();
+    }
+
+    /**
+     * 移动指针到下一个节点
+     */
+    @Override
+    public void moveNext() {
+        Node<E> pointer = this.pointer;
+        this.pointer = pointer.getNext();
+    }
+
+    /**
+     * 移动指针到上一个节点
+     */
+    @Override
+    public void movePrev() {
+        Node<E> pointer = this.pointer;
+        this.pointer = pointer.getNext();
+    }
+
+    /**
+     * @return 大小（有多少个{@link Node}节点加入了{@link PointerLinked}）
+     */
+    @Override
+    public int getSize() {
+        return this.size;
+    }
+
+}
