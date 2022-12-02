b031da31e97a00e29ac0a2ef716478ace429e688
RealDragonking <51237027+sqddk@users.noreply.github.com>
2022-12-02 17:22:12 +0800

    struct部分完成！基本数据结构ending

--- Cherry-Core/src/main/java/cn/cherry/core/engine/base/struct/Node.java
+++ Cherry-Core/src/main/java/cn/cherry/core/engine/base/struct/Node.java
@@ -4,7 +4,7 @@ package cn.cherry.core.engine.base.struct;
  * 可以被{@link PointerLinked}接口实现类使用的节点数据结构，提供访问、修改前一个节点{@link #prev}和后一个节点{@link #next}的方法<br/>
  * 注意，{@link Node}的值{@link #value}一旦被设置后将无法更改！
  *
- * @author realDragonKing
+ * @author realDragonKing、liehu3
  */
 public class Node<E> {
 
@@ -52,6 +52,9 @@ public class Node<E> {
 
     /**
      * 设置前、后节点
+     *
+     * @param prev 前一个节点
+     * @param next 后一个节点
      */
     public void set(Node<E> prev, Node<E> next) {
         this.prev = prev;
