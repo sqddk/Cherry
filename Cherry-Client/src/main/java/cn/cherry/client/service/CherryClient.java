a7f5206f68ea708d19f5802976fdf359fe580f8a
RealDragonking <51237027+sqddk@users.noreply.github.com>
2022-12-17 00:29:20 +0800

    Cherry-Client的v0.0.3推进度

--- Cherry-Client/src/main/java/cn/cherry/client/service/CherryClient.java
+++ Cherry-Client/src/main/java/cn/cherry/client/service/CherryClient.java
@@ -12,7 +12,6 @@ import org.apache.logging.log4j.Logger;
 
 import java.util.concurrent.*;
 
-import static io.netty.util.internal.ObjectUtil.checkPositive;
 
 /**
  * 与远程cherry定时任务调度服务端进行交互的客户端
@@ -37,38 +36,45 @@ public class CherryClient {
      * @param timeout 连接超时时间
      */
     private void connect(String host, int port, long timeout) {
-        checkPositive(timeout, "timeout");
-
         CompletableFuture<Boolean> waiter = new CompletableFuture<>();
 
-        new Thread(() -> {
-            Bootstrap bootstrap = new Bootstrap();
-            EventLoopGroup workerGroup = new NioEventLoopGroup(2);
-
-            try {
-                bootstrap.group(workerGroup)
-                        .channel(NioSocketChannel.class)
-                        .option(ChannelOption.TCP_NODELAY, true)
-                        .handler(new ClientInitializer());
-
-                ChannelFuture future = bootstrap.connect(host, port).sync();
-
-                this.channel = future.channel();
-                this.logger.info("与远程cherry服务端 " + this.channel.remoteAddress() + " 的连接已经建立！");
-                waiter.complete(true);
-
-                this.channel.closeFuture().sync();
-            } catch (Exception e) {
-                e.printStackTrace();
-            } finally {
-                workerGroup.shutdownGracefully();
-            }
-        }).start();
+        Thread thread = new Thread(() -> connect0(host, port, waiter));
+        thread.start();
 
         try {
             waiter.get(timeout, TimeUnit.MILLISECONDS);
         } catch (InterruptedException | ExecutionException | TimeoutException e) {
-            this.logger.error("连接目标服务端超时失败！");
+            this.logger.error("连接目标服务端因超时失败！");
+        }
+    }
+
+    /**
+     * 通过提供的host地址和端口，初始化客户端，尝试连接目标服务端
+     *
+     * @param host host地址
+     * @param port port端口
+     * @param waiter 超时等候器
+     */
+    private void connect0(String host, int port, CompletableFuture<Boolean> waiter) {
+        Bootstrap bootstrap = new Bootstrap();
+        EventLoopGroup workerGroup = new NioEventLoopGroup(2);
+
+        try {
+            bootstrap.group(workerGroup)
+                    .channel(NioSocketChannel.class)
+                    .option(ChannelOption.TCP_NODELAY, true)
+                    .handler(new ClientInitializer());
+
+            ChannelFuture future = bootstrap.connect(host, port).sync();
+
+            this.channel = future.channel();
+            this.logger.info("与远程cherry服务端 " + this.channel.remoteAddress() + " 的连接已经建立！");
+
+            this.channel.closeFuture().sync();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            workerGroup.shutdownGracefully();
         }
     }
 
